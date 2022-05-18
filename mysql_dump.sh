2f8fa7a995c29378ddfb0b62740126742e5f3a5f
Mike Tesliuk <mike@tesliuk.com>
2022-05-18 00:31:36 -0300

    Update mysql_dump.sh
    Separado esquema de backup com schema e dados
    Adicionada opcoes para backup de triggers e procedures

--- mysql_dump.sh
+++ mysql_dump.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
 
-MYSQL_USER=root
-MYSQL_PASS=SUA_SENHA
+MYSQL_USER=__BACKUP__USER
+MYSQL_PASS=__BACKUP__PASS
 MYSQL_DUMP_PARAMETERS="--all-databases"
+MYSQL_DUMP_SCHEMA_PARAMETERS="--routines --triggers --events --no-data "
+MYSQL_DUMP_DATA_PARAMETERS="--routines --events --triggers --no-create-info --no-create-db --single-transaction "
 
 MYSQL_DUMP_TYPE=$1
 
-DIFFDIR=/home/backup
-SERVIDOR=`hostname`
+# Defina abaixo o path para onde o backup será armazenado
+DIFFDIR=/mnt/BACKUP/BASES_NOVO/diff
+
+# Defina abaixo uma nomeclatura util para o servidor, aqui usaremos SERVIDOR_IP
+SERVIDOR_NOME="SERVER_NAME_AND_SERVER_IP"
+
+# Altere o ip abaixo para o ip do servidor em quest
+SERVIDOR_IP="127.0.0.1"
 
 MYMODULO="sql"
 
@@ -36,35 +44,40 @@ exec_limpeza(){
 
 
 # definincao da retencao
-RETENCAO=15
+RETENCAO=5
 
-exec_limpeza $RETENCAO $DIFFDIR $SERVIDOR
+exec_limpeza $RETENCAO $DIFFDIR $SERVIDOR_NOME
 
 exec_mysql_d_full(){
 
-	if [ ! -d $DIFFDIR/$(date +%Y)/$(date +%m)/$(date +%d)/$SERVIDOR/$MYMODULO ]; then
+	if [ ! -d $DIFFDIR/$(date +%Y)/$(date +%m)/$(date +%d)/$SERVIDOR_NOME/$MYMODULO ]; then
 
-		mkdir -p $DIFFDIR/$(date +%Y)/$(date +%m)/$(date +%d)/$SERVIDOR/$MYMODULO
-		echo "Criado Diretorio: $DIFFDIR/$(date +%Y)/$(date +%m)/$(date +%d)/$SERVIDOR/$MYMODULO"
+		mkdir -p $DIFFDIR/$(date +%Y)/$(date +%m)/$(date +%d)/$SERVIDOR_NOME/$MYMODULO
+		echo "Criado Diretorio: $DIFFDIR/$(date +%Y)/$(date +%m)/$(date +%d)/$SERVIDOR_NOME/$MYMODULO"
 	fi
 
 
-	/usr/bin/mysqldump -u${MYSQL_USER} -p${MYSQL_PASS} ${MYSQL_DUMP_PARAMETERS} > $DIFFDIR/$(date +%Y)/$(date +%m)/$(date +%d)/$SERVIDOR/$MYMODULO/database.sql
-	/usr/bin/gzip $DIFFDIR/$(date +%Y)/$(date +%m)/$(date +%d)/$SERVIDOR/$MYMODULO/database.sql
+	echo "[$(date)] Dumping database start: ALL_DATABASES"
+	/usr/bin/mysqldump -u${MYSQL_USER} -p${MYSQL_PASS} -h${SERVIDOR_IP} ${MYSQL_DUMP_PARAMETERS} > $DIFFDIR/$(date +%Y)/$(date +%m)/$(date +%d)/$SERVIDOR_NOME/$MYMODULO/database.sql
+	/usr/bin/gzip $DIFFDIR/$(date +%Y)/$(date +%m)/$(date +%d)/$SERVIDOR_NOME/$MYMODULO/database.sql
+	echo "[$(date)] Dumping database finish: ALL_DATABASES"
 
 }
 
 exec_mysql_d_databases(){
-	if [ ! -d $DIFFDIR/$(date +%Y)/$(date +%m)/$(date +%d)/$SERVIDOR/$MYMODULO ]; then
+	if [ ! -d $DIFFDIR/$(date +%Y)/$(date +%m)/$(date +%d)/$SERVIDOR_NOME/$MYMODULO ]; then
                 
-                mkdir -p $DIFFDIR/$(date +%Y)/$(date +%m)/$(date +%d)/$SERVIDOR/$MYMODULO
-                echo "Criado Diretorio: $DIFFDIR/$(date +%Y)/$(date +%m)/$(date +%d)/$SERVIDOR/$MYMODULO"
+                mkdir -p $DIFFDIR/$(date +%Y)/$(date +%m)/$(date +%d)/$SERVIDOR_NOME/$MYMODULO
+                echo "Criado Diretorio: $DIFFDIR/$(date +%Y)/$(date +%m)/$(date +%d)/$SERVIDOR_NOME/$MYMODULO"
         fi
 
-	for DATABASES in $(/usr/bin/mysql -u${MYSQL_USER} -p${MYSQL_PASS} -e "show databases" | sed 1d); do
-		echo "Dumping database: $DATABASES"
-		/usr/bin/mysqldump -u${MYSQL_USER} -p${MYSQL_PASS} --database ${DATABASES} > $DIFFDIR/$(date +%Y)/$(date +%m)/$(date +%d)/$SERVIDOR/$MYMODULO/${DATABASES}.sql
-        	/usr/bin/gzip $DIFFDIR/$(date +%Y)/$(date +%m)/$(date +%d)/$SERVIDOR/$MYMODULO/${DATABASES}.sql
+	for DATABASES in $(/usr/bin/mysql -u${MYSQL_USER} -p${MYSQL_PASS} -h${SERVIDOR_IP} -e "show databases" | sed 1d); do
+		echo "[$(date)] Dumping database start: $DATABASES"
+		/usr/bin/mysqldump -u${MYSQL_USER} -p${MYSQL_PASS} -h${SERVIDOR_IP} --databases ${DATABASES} ${MYSQL_DUMP_SCHEMA_PARAMETERS} > $DIFFDIR/$(date +%Y)/$(date +%m)/$(date +%d)/$SERVIDOR_NOME/$MYMODULO/${DATABASES}_schema.sql
+		/usr/bin/mysqldump -u${MYSQL_USER} -p${MYSQL_PASS} -h${SERVIDOR_IP} --databases ${DATABASES} ${MYSQL_DUMP_DATA_PARAMETERS} > $DIFFDIR/$(date +%Y)/$(date +%m)/$(date +%d)/$SERVIDOR_NOME/$MYMODULO/${DATABASES}_data.sql
+        	/usr/bin/gzip $DIFFDIR/$(date +%Y)/$(date +%m)/$(date +%d)/$SERVIDOR_NOME/$MYMODULO/${DATABASES}_schema.sql
+        	/usr/bin/gzip $DIFFDIR/$(date +%Y)/$(date +%m)/$(date +%d)/$SERVIDOR_NOME/$MYMODULO/${DATABASES}_data.sql
+		echo "[$(date)] Dumping database finish: $DATABASES"
 
 	done
 }
@@ -74,7 +87,9 @@ case $MYSQL_DUMP_TYPE in
 		exec_mysql_d_full
 	;;
 	'DATABASES')
-		exec mysql_d_databases
+		exec_mysql_d_databases
+	;;
+	'*')
+		echo "Utilize mysql_dump.sh [FULL|DATABASES]"
 	;;
 esac
-
